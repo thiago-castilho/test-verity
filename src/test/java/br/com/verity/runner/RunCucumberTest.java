712c80964ec831e2ab6edabc74f1305fb3d0aa80
Thiago Castilho <gft.thiago.castilho@br.experian.com>
2019-12-04 11:25:28 -0300

    Adding better report to the tests

--- src/test/java/br/com/verity/runner/RunCucumberTest.java
+++ src/test/java/br/com/verity/runner/RunCucumberTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-html-reports"},
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"},
 				 glue = "br/com/verity/gherkin",
 				 features = "features")
 public class RunCucumberTest {}
